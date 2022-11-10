07f76dc5c5b8bc772c5962b3736279974ffad486
Ubuntu <ubuntu@ip-172-31-14-168.ec2.internal>
2022-11-10 05:33:39 +0000

    [Add] sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    prams.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
